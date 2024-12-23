4c6dfab213faafc0871827aaf1aff2dd74057810
Vedant <borodevedant4@gmail.com>
2024-12-23 23:55:06 +0530

    add sendEmail routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 import express from "express"
+import { sendMail } from "./controllers/sendmail.js"
 
 
 const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+app.post("/sendMail", sendMail)
 
 export {app}
