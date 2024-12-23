401723ca60f2311993d1d19c7a5a2a4e68242355
Vedant <borodevedant4@gmail.com>
2024-12-24 02:00:16 +0530

    add CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 import express from "express"
 import { sendMail } from "./controllers/sendmail.js"
-
+import cors from "cors"
 
 const app = express()
 
+const allowedOrigin = process.env.CORS_ORIGIN
+
+const corsOptions = {
+  origin: allowedOrigin,
+  methods: ['GET', 'POST'],  
+};
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors(corsOptions))
 
 app.post("/sendMail", sendMail)
 
