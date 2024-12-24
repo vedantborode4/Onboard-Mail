005333a594d81183976708a953989007c7c7bc7f
Vedant <borodevedant4@gmail.com>
2024-12-24 20:29:17 +0530

    fix bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,10 @@ const app = express()
 const allowedOrigin = process.env.CORS_ORIGIN
 
 const corsOptions = {
-  origin: [
-    'https://onboard-mail.vercel.app',
-    'http://localhost:5173' // For local development
-  ],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  credentials: true
+  origin: allowedOrigin,  
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
-
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors(corsOptions))
